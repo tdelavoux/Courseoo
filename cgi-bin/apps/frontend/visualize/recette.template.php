0da60f6f3935ca34684ccb44385935a1aba52887
Thibault Del <thibault.delavoux@gmail.com>
2020-06-24 23:20:41 +0200

    ajout page visu des recettes

--- cgi-bin/apps/frontend/visualize/recette.template.php
+++ cgi-bin/apps/frontend/visualize/recette.template.php
@@ -1,5 +1,25 @@
-
+<?php 
+    $recepeInfo     = \Page::get('recepeInfo'); 
+    $ingredients    = \Page::get('ingredients'); 
+?>
 <div class="container">
-
+    
+ <!-------------------- ENTETE -------------------------------->
+    <div class="row jumbotron">
+        <h1 class=""><?php echo \Db::decode($recepeInfo['nom']); ?></h1>
+    </div>
+    
+    <!-------------------- CORPS  -------------------------------->
+    
+    <div class="row justify-content-md-center">
+        <div class="col-md-8">
+            <div class="list-group list-cust">
+                <span class="list-group-item active">Ingrédients de la recette</span>
+                <?php foreach($ingredients as $ingredient) : ?>
+                    <span  class="list-group-item"><?php echo $ingredient['quantite'] . $ingredient['uniteMesure'] . ' ' .  \Db::decode($ingredient['ingredient']); ?></span>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
  
 </div>
